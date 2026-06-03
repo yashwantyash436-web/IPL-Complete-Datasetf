ae8ebc1c2327d944bce4ee275793b1083da013bf
yashwantyash436-web <yashwantyash436@gmail.com>
2026-06-03 16:20:51 +0530

    Create analysis.py

---
+++ notebook/analysis.py
@@ -0,0 +1,12 @@
+# =========================================
+# IPL Analytics Project
+# analysis.py
+# =========================================
+
+# Import Libraries
+import os
+import zipfile
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
